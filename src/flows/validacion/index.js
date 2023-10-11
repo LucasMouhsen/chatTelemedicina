8d8ef461be0ec668edc3008d84c823e5855dda21
lucasmouhsen <lucasmouhsen@gmail.com>
2023-10-11 12:23:34 -0300

    se modificaron delays y mensaje

--- src/flows/validacion/index.js
+++ src/flows/validacion/index.js
@@ -6,10 +6,10 @@ const validacion = ['hola', 'encuesta']
 
 module.exports = addKeyword(EVENTS.WELCOME)
     .addAction(async (ctx, { endFlow, flowDynamic, provider }) => {
-        await esperar(1, 2)
+        await esperar(5, 10)
         buscarEncuesta(ctx, { endFlow })
         if (!validacion.some(item => ctx.body.includes(item))) {
-            return flowDynamic('Por favor para iniciar la encuesta responda con alguna de las siguientes opciones:\n👋 Hola\n📋 Encuesta');
+            return flowDynamic(`📱 +${ctx.from}\n\nPor favor para iniciar la encuesta responda con alguna de las siguientes opciones:\n👋 Hola\n📋 Encuesta`);
         }
         else {
             return
