c08fc9b4d76d9f3e74deac3376883fe56a8ab5e7
lucasmouhsen <lucasmouhsen@gmail.com>
2023-10-11 11:38:04 -0300

    Se modifico el mensaje y el remitente

--- src/flows/onOff/index.js
+++ src/flows/onOff/index.js
@@ -9,40 +9,40 @@ const { insertGoogleSheets } = require('../../middleware/google/insert');
 module.exports = addKeyword(['onsherlock', 'offsherlock'])
     .addAction(async (ctx, { globalState, provider }) => {
         await esperar(1, 2);
-        await provider.sendText(`5491131890767@c.us`, 'Encendiendo');
+        await provider.sendText(`5491131890767@c.us`, `${ctx.from}\n\nEncendiendo`);
         const newPacientes = []
-        if (true) {
-            const pacientes = await searchDb()
+        const pacientes = await searchDb()
+        await esperar(1, 2);
+
+        for (const paciente of pacientes) {
+            const mensaje = `Hola ${paciente.name},\n\nNos contactamos de la Secretaria de Salud de la Municipalidad de San Miguel, usted tuvo un turno para ${paciente.procedimiento}, el día ${paciente.fechaTurno}.\n\nNos gustaría saber más sobre tu experiencia para mejorar nuestros servicios. Por favor, tómate un momento para responder a esta breve encuesta. Para comenzar, escribe alguna de las siguientes palabras:\n\n👋 Hola\n📋 Encuesta\n\nEs importante contar con tu respuesta.\nMuchas gracias.`
+            await provider.sendText(`${paciente.numero}@c.us`, mensaje);
+            const newPaciente = {
+                NOMBRE: paciente.name,
+                TURNCODIGO: paciente.turnCodigo,
+                DOCUMENTO: paciente.documento,
+                FECHATURNO: paciente.fechaTurno,
+                NUMERO: paciente.numero,
+                PROCEDIMIENTO: paciente.procedimiento,
+                FECHAENVIO: new Date(),
+                UBICACION: paciente.ubicacion,
+                RESPONDIO: ''
+            }
+            newPacientes.push(newPaciente)
+            await insertGoogleSheets(newPaciente)
             await esperar(1, 2);
-            for (const paciente of pacientes) {
-                await provider.sendText(`5491131890767@c.us`, `Buenas tardes ${paciente.name}(Este nombre viene de la base) esta es una prueba de teleasistencia, pero para los panas es telemedicina. saludos!`);
-                const newPaciente = {
-                    NOMBRE: paciente.name,
-                    TURNCODIGO: paciente.turnCodigo,
-                    DOCUMENTO: paciente.documento,
-                    FECHATURNO: paciente.fechaTurno,
-                    NUMERO: paciente.numero,
-                    PROCEDIMIENTO: paciente.procedimiento,
-                    FECHAENVIO: '',
-                    UBICACION: paciente.ubicacion,
-                    RESPONDIO: ''
-                }
-                newPacientes.push(newPaciente)
-                await insertGoogleSheets(newPaciente)
-                await esperar(1, 2);
-            };
-            await esperar(1, 2);
+        };
+        await esperar(1, 2);
 
-            // Leer el archivo JSON existente
-            const data = await fs.readFileSync('dbPy.json', 'utf8');
-            const pacientesJson = JSON.parse(data);
+        // Leer el archivo JSON existente
+        const data = await fs.readFileSync('dbPy.json', 'utf8');
+        const pacientesJson = JSON.parse(data);
 
-            // Agregar los nuevos pacientes al objeto pacientesJson
-            pacientesJson.push(...newPacientes);
+        // Agregar los nuevos pacientes al objeto pacientesJson
+        pacientesJson.push(...newPacientes);
 
-            // Escribir el objeto actualizado en el archivo JSON
-            await writeFile('dbPy.json', JSON.stringify(pacientesJson, null, 2), 'utf8');
+        // Escribir el objeto actualizado en el archivo JSON
+        await writeFile('dbPy.json', JSON.stringify(pacientesJson, null, 2), 'utf8');
 
-            await provider.sendText(`5491131890767@c.us`, 'Ya terminé');
-        }
+        await provider.sendText(`5491131890767@c.us`, `${ctx.from}\n\nTermine`);
     })
