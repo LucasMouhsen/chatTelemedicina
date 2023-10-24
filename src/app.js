93196f4952023697acd86c1ba03066336409b30a
lucasmouhsen <lucasmouhsen@gmail.com>
2023-10-24 08:43:43 -0300

    ADD: process.env.PORT

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const { createBot, createProvider, createFlow } = require('@bot-whatsapp/bot');
 const QRPortalWeb = require('@bot-whatsapp/portal')
